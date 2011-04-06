2ed268722611e7a563944879c1b6c8b17daa747b
groovecoder <luke.crouch@gmail.com>
2011-04-06 11:19:26 -0500

    work in django<=1.2.4

--- appetizer/views.py
+++ appetizer/views.py
@@ -3,6 +3,11 @@ from django.contrib.sites.models import Site, get_current_site
 from django.shortcuts import render_to_response
 
 def manifest(request, template='appetizer/manifest.json'):
-    current_site = get_current_site()
+    current_site = None
+    try: get_current_site
+    except NameError:
+        current_site = Site.objects.get_current()
+    else:
+        current_site = get_current_site()
     site_name = current_site.name
     return render_to_response(template, locals(), mimetype='application/x-web-app-manifest+json')
