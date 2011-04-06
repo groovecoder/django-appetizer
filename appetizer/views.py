43b150ac83105b263aab96dce84e1a4d984bb66b
groovecoder <luke.crouch@gmail.com>
2011-04-06 10:50:06 -0500

    oops, need to import render_to_response

--- appetizer/views.py
+++ appetizer/views.py
@@ -1,2 +1,4 @@
+from django.shortcuts import render_to_response
+
 def manifest(request, template='appetizer/manifest.json'):
     return render_to_response(template, mimetype='application/x-web-app-manifest+json')
