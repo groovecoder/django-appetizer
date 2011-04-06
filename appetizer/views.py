3e213142ce0b03338841ef989a0cd7e9246b2ad5
groovecoder <luke.crouch@gmail.com>
2011-04-06 11:27:08 -0500

    try imports for django 1.2.x

--- appetizer/views.py
+++ appetizer/views.py
@@ -1,13 +1,15 @@
 from django.conf import settings
-from django.contrib.sites.models import Site, get_current_site
+
+try:
+    from django.contrib.sites.models import get_current_site
+    current_site = get_current_site
+except ImportError:
+    from django.contrib.sites.models import Site
+    current_site = Site.objects.get_current
+
 from django.shortcuts import render_to_response
 
 def manifest(request, template='appetizer/manifest.json'):
-    current_site = None
-    try: get_current_site
-    except NameError:
-        current_site = Site.objects.get_current()
-    else:
-        current_site = get_current_site()
-    site_name = current_site.name
+    site = current_site()
+    site_name = site.name
     return render_to_response(template, locals(), mimetype='application/x-web-app-manifest+json')
